8f65e4405f2a772b9596230a2ab2c002c2c48259
Andreas Isaak <andy.jared@googlemail.com>
2014-02-26 13:33:44 +0100

    Contao 3 compatibility

---
+++ system/modules/backendUserHistory/config/autoload.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ *
+ * Copyright (c) 2005-2014 Leo Feyer
+ *
+ * @package BackendUserHistory
+ * @link    https://contao.org
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ */
+
+
+/**
+ * Register the classes
+ */
+ClassLoader::addClasses(array
+(
+	'BackendUserHistory' => 'system/modules/backendUserHistory/BackendUserHistory.php',
+));
